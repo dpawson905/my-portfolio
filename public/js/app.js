f0bb9dfc3875c27a70bc60a76c7d3b6204348304
Darrell Pawson <dpawson905@gmail.com>
2022-03-18 23:20:45 -0400

    work on about and work section:

--- public/js/app.js
+++ public/js/app.js
@@ -6,18 +6,17 @@ function ready(fn) {
   }
 }
 
-const typewriter1 = ( className, string) => {
-  const type1 = document.querySelector(className);
-  let typewriter = new Typewriter(type1, {
-    loop: false,
-    delay: 75
-  });
-  typewriter
-  .pauseFor(300)
-  .typeString(string)
-  .start()
-}
-
 ready(function () {
+  const typewriter1 = ( className, string) => {
+    const type1 = document.querySelector(className);
+    let typewriter = new Typewriter(type1, {
+      loop: false,
+      delay: 75
+    });
+    typewriter
+    .start()
+    .pauseFor(300)
+    .typeString(string)
+  }
   typewriter1(".sub-heading", "is()");
 });
